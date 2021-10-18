74e933765f175cd0a455c8c2dcdfcee2946479e8
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-18 14:51:05 -0400

    Starts implementing jest docs test example

--- testing.js
+++ testing.js
@@ -1,3 +1,12 @@
-function add(a,b){
-   return a+b;
+function sum(a, b) {
+   return a + b;
 }
+
+//module.exports = sum; <---- commented out because we are working on the same file
+   // typically your tests will be in a separate file
+   
+// the test is below
+
+test('adds 1 + 2 to equal 3', () => {
+   expect(sum(1, 2)).toBe(3);
+});
